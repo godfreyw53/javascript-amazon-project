158f4211f2b6792881f7b08bf8147a6640b166a3
godfreyw53 <godfreyw53@gmail.com>
2026-01-04 02:29:25 +0300

    convert from promises form async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,6 +6,23 @@ import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
+
+ async  function loadPage(){
+  
+
+  await loadProductsFetch();
+
+  await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+renderCheckoutHeader();
+renderOrderSummary();
+renderPaymentSummary();
+}
+loadPage();
+/*
 Promise.all([
   loadProductsFetch(),
  new Promise((resolve)=>{
@@ -20,6 +37,7 @@ Promise.all([
 renderOrderSummary();
 renderPaymentSummary();
 });
+*/
  /*
 new Promise((resolve)=>{
  loadProducts(()=>{
